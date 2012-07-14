3eda360d7099e1897b354fbff73b903cc1ece18f
Eric Kim <erickim555@gmail.com>
2012-07-14 15:37:01 -0700

    Small edit of demo case

--- opencount/pixel_reg/eric_outofbounds/demo.py
+++ opencount/pixel_reg/eric_outofbounds/demo.py
@@ -10,11 +10,13 @@ import shared
 img1path = '_err_img1.png'
 
 def main():
-    bb = [0, 30, 0, 16]
     regions = ['extracted_digitpatches/precinct/0_exemplar.png',
                'extracted_digitpatches/precinct/1_exemplar.png', 
                'extracted_digitpatches/precinct/2_exemplar.png']
     img1 = shared.standardImread(img1path, flatten=True)
+    (h,w) = img1.shape
+    bb = [0, h-1, 0, w-1]
+    print "bb is:", bb
     matches = shared.find_patch_matchesV1(img1, bb, regions, threshold=0.7)
     print 'Found {0} number of matches.'.format(len(matches))
 
