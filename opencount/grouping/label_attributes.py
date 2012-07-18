e5eecebd7f9121fb2c50ec7b6020ba0c375c81d0
Eric Kim <erickim555@gmail.com>
2012-07-17 19:09:34 -0700

    Minor refactor.

--- opencount/grouping/label_attributes.py
+++ opencount/grouping/label_attributes.py
@@ -115,157 +115,6 @@ together."
         self.Close()
         self.ondone(results)
 
-class LabelPanel(wx.lib.scrolledpanel.ScrolledPanel):
-    """
-    A panel that allows you to, given a set of images I, give a text
-    label to each image. Outputs to an output file.
-    """
-    STATE_FILE = '_labelpanelstate.p'
-
-    def __init__(self, parent, *args, **kwargs):
-        wx.lib.scrolledpanel.ScrolledPanel.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
-        
-        self.imagelabels = {}   # maps {imagepath: str label}
-
-        self.imagepaths = []  # ordered list of imagepaths
-        self.cur_imgidx = 0  # which image we're currently at
-
-        self.outpath = 'labelpanelout.csv'
-
-        self._init_ui()
-        self.Fit()
-
-    def _init_ui(self):
-        self.sizer = wx.BoxSizer(wx.VERTICAL)
-        self.SetSizer(self.sizer)
-
-        self.sizer2 = wx.BoxSizer(wx.HORIZONTAL)
-        self.imgpatch = wx.StaticBitmap(self)
-
-        labeltxt = wx.StaticText(self, label='Label:')
-        self.inputctrl = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)
-        self.inputctrl.Bind(wx.EVT_TEXT_ENTER, self.onInputEnter, self.inputctrl)
-        nextbtn = wx.Button(self, label="Next")
-        prevbtn = wx.Button(self, label="Previous")
-        nextbtn.Bind(wx.EVT_BUTTON, self.onButton_next)
-        prevbtn.Bind(wx.EVT_BUTTON, self.onButton_prev)
-        inputsizer = wx.BoxSizer(wx.HORIZONTAL)
-        inputsizer.Add(labeltxt)
-        inputsizer.Add(self.inputctrl)
-
-        sizer3 = wx.BoxSizer(wx.VERTICAL)
-        sizer3.Add(inputsizer)
-        sizer3.Add(nextbtn)
-        sizer3.Add(prevbtn)
-
-        self.sizer2.Add(self.imgpatch, proportion=0)
-        self.sizer2.Add(sizer3, proportion=0)
-        
-        self.sizer.Add(self.sizer2, proportion=1, flag=wx.EXPAND)
-
-    def onInputEnter(self, evt):
-        """ Triggered when the user hits 'enter' when inputting text
-        """
-        curimgpath = self.imagepaths[self.cur_imgidx]
-        cur_val = self.inputctrl.GetValue()
-        self.imagelabels[curimgpath] = cur_val
-        if (self.cur_imgidx+1) >= len(self.imagepaths):
-            return
-        self.display_img(self.cur_imgidx + 1)
-
-    def onButton_next(self, evt):
-        if (self.cur_imgidx+1) >= len(self.imagepaths):
-            return
-        else:
-            self.display_img(self.cur_imgidx + 1)
-            
-    def onButton_prev(self, evt):
-        if self.cur_imgidx <= 0:
-            return
-        else:
-            self.display_img(self.cur_imgidx - 1)
-
-    def start(self, imageslist, outfile='labelpanelout.csv'):
-        """Given a dict of imagepaths to label, set up the UI, and
-        allow the user to start labeling things.
-        Input:
-            lst imageslist: list of image paths
-            outfile: Output file to write results to.
-        """
-        for imgpath in imageslist:
-            assert imgpath not in self.imagelabels
-            assert imgpath not in self.imagepaths
-            self.imagelabels[imgpath] = ''
-            self.imagepaths.append(imgpath)
-
-        self.cur_imgidx = 0
-        self.display_img(self.cur_imgidx)
-        self.Fit()
-
-    def restore_session(self, statefile=None):
-        """ Tries to restore the state of a previous session. If this
-        fails (say, the internal state file was deleted), then this
-        will return False. If this happens, then you should just call
-        self.start().
-        """
-        if statefile == None:
-            statefile = LabelPanel.STATE_FILE
-        if not os.path.exists(LabelPanel.STATE_FILE):
-            return False
-        state = pickle.load(open(LabelPanel.STATE_FILE, 'rb'))
-        imagelabels = state['imagelabels']
-        imagepaths = state['imagepaths']
-        self.imagelabels = imagelabels
-        self.imagepaths = imagepaths
-        self.cur_imgidx = 0
-        self.display_img(self.cur_imgidx, no_overwrite=True)
-        self.Fit()
-        return True
-
-    def save_session(self, statefile=None):
-        """ Saves the current state of the current session. """
-        if statefile == None:
-            statefile = LabelPanel.STATE_FILE
-        state = {}
-        state['imagelabels'] = self.imagelabels
-        state['imagepaths'] = self.imagepaths
-        f = open(LabelPanel.STATE_FILE, 'wb')
-        pickle.dump(state, f)
-        f.close()
-
-    def display_img(self, idx, no_overwrite=False):
-        """Displays the image at idx, and allow the user to start labeling
-        it.
-        """
-        if not (idx < len(self.imagepaths)):
-            pdb.set_trace()
-        assert idx < len(self.imagepaths)
-        if not no_overwrite:
-            # First, store current input into our dict
-            old_imgpath = self.imagepaths[self.cur_imgidx]
-            cur_input = self.inputctrl.GetValue()
-            self.imagelabels[old_imgpath] = cur_input
-
-        self.cur_imgidx = idx
-        imgpath = self.imagepaths[self.cur_imgidx]
-        bitmap = wx.Bitmap(imgpath, type=wx.BITMAP_TYPE_PNG)
-        self.imgpatch.SetBitmap(bitmap)
-
-        self.inputctrl.SetValue(self.imagelabels[imgpath])
-        self.Fit()
-        
-    def export_labels(self):
-        """ Exports all labels to an output csvfile. """
-        f = open(self.outpath, 'w')
-        header = ('imgpath', 'label')
-        dictwriter = csv.DictWriter(f, header)
-        util_gui._dictwriter_writeheader(f, header)
-        for imgpath, label in self.imagelabels.iteritems():
-            row = {'imgpath': imgpath, 'label': label}
-            dictwriter.write_row(row)
-        f.close()
-
 class LabelAttributesPanel(wx.lib.scrolledpanel.ScrolledPanel):
     """ A panel that will be integrated directly into OpenCount. """
     def __init__(self, parent, *args, **kwargs):
@@ -417,146 +266,154 @@ class LabelAttributesPanel(wx.lib.scrolledpanel.ScrolledPanel):
     def checkCanMoveOn(self):
         """ Return True if the user can move on, False otherwise. """
         return True
+
+class LabelPanel(wx.lib.scrolledpanel.ScrolledPanel):
+    """
+    A panel that allows you to, given a set of images I, give a text
+    label to each image. Outputs to an output file.
+    """
+    STATE_FILE = '_labelpanelstate.p'
+
+    def __init__(self, parent, *args, **kwargs):
+        wx.lib.scrolledpanel.ScrolledPanel.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
         
-'''
-class LabelAttributesPanel(LabelContest):
-    """
-    This class is one big giant hack of a monkey-patch.
-    """
-    def set_attrgroup_results(self, groupresults):
-        """ Given the result of grouping the attribute patches, update
-        my self.equivs data structures. groupresults is a dict:
-            {grouplabel: list of GroupClass objects}
+        self.imagelabels = {}   # maps {imagepath: str label}
+
+        self.imagepaths = []  # ordered list of imagepaths
+        self.cur_imgidx = 0  # which image we're currently at
+
+        self.outpath = 'labelpanelout.csv'
+
+        self._init_ui()
+        self.Fit()
+
+    def _init_ui(self):
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(self.sizer)
+
+        self.sizer2 = wx.BoxSizer(wx.HORIZONTAL)
+        self.imgpatch = wx.StaticBitmap(self)
+
+        labeltxt = wx.StaticText(self, label='Label:')
+        self.inputctrl = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)
+        self.inputctrl.Bind(wx.EVT_TEXT_ENTER, self.onInputEnter, self.inputctrl)
+        nextbtn = wx.Button(self, label="Next")
+        prevbtn = wx.Button(self, label="Previous")
+        nextbtn.Bind(wx.EVT_BUTTON, self.onButton_next)
+        prevbtn.Bind(wx.EVT_BUTTON, self.onButton_prev)
+        inputsizer = wx.BoxSizer(wx.HORIZONTAL)
+        inputsizer.Add(labeltxt)
+        inputsizer.Add(self.inputctrl)
+
+        sizer3 = wx.BoxSizer(wx.VERTICAL)
+        sizer3.Add(inputsizer)
+        sizer3.Add(nextbtn)
+        sizer3.Add(prevbtn)
+
+        self.sizer2.Add(self.imgpatch, proportion=0)
+        self.sizer2.Add(sizer3, proportion=0)
+        
+        self.sizer.Add(self.sizer2, proportion=1, flag=wx.EXPAND)
+
+    def onInputEnter(self, evt):
+        """ Triggered when the user hits 'enter' when inputting text
         """
-        def get_cid(blankpath, patchpath):
-            """Determine the c_id of this attribute patch. """
-            # Assumes that patchpath is of the form:
-            #    <projdir_path>/extract_attrs_templates/<ATTR_STR>/*.png
-            attrs_str = os.path.basename(os.path.split(patchpath)[0])
-            return self.cid_map[(blankpath, attrs_str)]
-        maps = {} # maps {grouplabel: list of (bid, cid)}
-        # First, merge each group in groupresults into maps
-        for grouplabel, groups in groupresults.iteritems():
-            if common.is_digit_grouplabel(grouplabel, self.proj):
-                continue
-            for group in groups:
-                for (blankpath, rankedlist, patchpath) in group.elements:
-                    bid = self.bid_map[blankpath]
-                    cid = get_cid(blankpath, patchpath)
-                    maps.setdefault(grouplabel, []).append((bid, cid))
-        for grouplabel, tups in maps.iteritems():
-            self.equivs.append(tups)
-        self.multibox_contests = []
-        self.has_equiv_classes = True
+        curimgpath = self.imagepaths[self.cur_imgidx]
+        cur_val = self.inputctrl.GetValue()
+        self.imagelabels[curimgpath] = cur_val
+        if (self.cur_imgidx+1) >= len(self.imagepaths):
+            return
+        self.display_img(self.cur_imgidx + 1)
 
-    def gatherData(self):
-        self.groupedtargets = []
-    
-        # attrdata is a list of dicts (marshall'd AttributeBoxes)
-        attrdata = pickle.load(open(self.proj.ballot_attributesfile))
-        attrdata = no_digitattrs(attrdata)
-        frontback = pickle.load(open(self.proj.frontback_map))
+    def onButton_next(self, evt):
+        if (self.cur_imgidx+1) >= len(self.imagepaths):
+            return
+        else:
+            self.display_img(self.cur_imgidx + 1)
+            
+    def onButton_prev(self, evt):
+        if self.cur_imgidx <= 0:
+            return
+        else:
+            self.display_img(self.cur_imgidx - 1)
 
-        self.sides = [x['side'] for x in attrdata]
-        self.types = [x['attrs'].keys()[0] for x in attrdata]
-        self.is_digitbased = [x['is_digitbased'] for x in attrdata]
-        self.is_tabulationonly = [x['is_tabulationonly'] for x in attrdata]
+    def start(self, imageslist, outfile='labelpanelout.csv'):
+        """Given a dict of imagepaths to label, set up the UI, and
+        allow the user to start labeling things.
+        Input:
+            lst imageslist: list of image paths
+            outfile: Output file to write results to.
+        """
+        for imgpath in imageslist:
+            assert imgpath not in self.imagelabels
+            assert imgpath not in self.imagepaths
+            self.imagelabels[imgpath] = ''
+            self.imagepaths.append(imgpath)
 
-        width, height = self.proj.imgsize
-        self.dirList = []
-        curbid = 0
-        # bid -> ballot id
-        bid_map = {}  # maps {str ballotpath: int b_id}
-        for dirpath, dirnames, filenames in os.walk(self.proj.blankballots_straightdir):
-            for imgname in [f for f in filenames if util_gui.is_image_ext(f)]:
-                ballotpath = os.path.join(dirpath, imgname)
-                self.dirList.append(ballotpath)
-                assert ballotpath not in bid_map
-                bid_map[ballotpath] = curbid
-                curbid += 1
-        self.bid_map = bid_map
-        # cid -> contest id
-        cid_map = {} # maps {(str ballotpath, str attrs): int c_id}
-        for i,f in enumerate(self.dirList):
-            thisballot = []
-            for curcid,at in enumerate(attrdata):
-                if at['side'] == frontback[os.path.abspath(f)]:
-                    assert f not in cid_map
-                    attrs = tuple(sorted(at['attrs'].keys()))
-                    attrs_str = '_'.join(attrs)
-                    cid_map[(f, attrs_str)] = curcid
-                    thisballot.append([(curcid, 0,
-                                        int(round(at['x1']*width)),
-                                        int(round(at['y1']*height)),
-                                        int(round(at['x2']*width)),
-                                        int(round(at['y2']*height)))])
-            self.groupedtargets.append(thisballot)
-        print "CID_MAP", cid_map
-        print "GRTARGS", self.groupedtargets
-        self.groupedtargets_back = self.groupedtargets
-        self.cid_map = cid_map
+        self.cur_imgidx = 0
+        self.display_img(self.cur_imgidx)
+        self.Fit()
 
-        self.template_width, self.template_height = self.proj.imgsize
-
-    def unsubscribe_pubsubs(self):
-        pass
-
-    def setupBoxes(self):
-        self.boxes = []
-        for each in self.groupedtargets:
-            bb = []
-            for contest in each:
-                id,_,l,u,r,d = contest[0]
-                bb.append((id,l,u,r,d))
-            self.boxes.append(bb)
-        self.groupedtargets = [[[]]*len(x) for x in self.groupedtargets]
-
-        # EVEN BIGGER HACK!!!
-        self.text = {}
-        for i,each in enumerate(self.boxes):
-            for x in each:
-                print "SETTING TEXT", i, x[0]
-                self.text[i,x[0]] = []
-                self.voteupto[i,x[0]] = 1
-        print self.text
-        if os.path.exists(self.proj.attr_internal):
-            self.text = pickle.load(open(self.proj.attr_internal))
-        # </hack>
-
-    def addText(self):
-        LabelContest.addText(self)
-        name = self.types[self.count]
-        self.contesttitle.SetLabel("Attribute Value (%s)"%name)
-
-    def save(self):
-        self.saveText(removeit=False)
-        print "TEXT", self.text
-        if not os.path.exists(self.proj.patch_loc_dir):
-            os.mkdir(self.proj.patch_loc_dir)
-        pickle.dump(self.text, open(self.proj.attr_internal, "w"))
-        for ballot in range(len(self.dirList)):
-            vals = sorted([(k,v) for k,v in self.text.items() if k[0] == ballot])
-            name = os.path.splitext(os.path.split(self.dirList[ballot])[-1])[0]+"_patchlocs.csv"
-            name = os.path.join(self.proj.patch_loc_dir, name)
-            print "MAKING", name
-            out = csv.writer(open(name, "w"))
-            out.writerow(["imgpath","id","x","y","width","height","attr_type",
-                          "attr_val","side","is_digitbased","is_tabulationonly"])
-            out.writerow([os.path.abspath(self.dirList[ballot]), DUMMY_ROW_ID,0,0,0,0,"_dummy_","_dummy_","_dummy_","_dummy_"])
-            for uid,each in enumerate(vals):
-                pos = self.groupedtargets_back[ballot][uid][0]
-                print "POS IS", pos, "EACH", each
-                value = "_none_" if each[1] == [] else each[1][0]
-                out.writerow([os.path.abspath(self.dirList[ballot]),
-                              uid, pos[2], pos[3],
-                              pos[4]-pos[2], pos[5]-pos[3],
-                              self.types[uid], value, self.sides[uid], self.is_digitbased[uid], self.is_tabulationonly[uid]])
-    def validate_outputs(self):
+    def restore_session(self, statefile=None):
+        """ Tries to restore the state of a previous session. If this
+        fails (say, the internal state file was deleted), then this
+        will return False. If this happens, then you should just call
+        self.start().
+        """
+        if statefile == None:
+            statefile = LabelPanel.STATE_FILE
+        if not os.path.exists(LabelPanel.STATE_FILE):
+            return False
+        state = pickle.load(open(LabelPanel.STATE_FILE, 'rb'))
+        imagelabels = state['imagelabels']
+        imagepaths = state['imagepaths']
+        self.imagelabels = imagelabels
+        self.imagepaths = imagepaths
+        self.cur_imgidx = 0
+        self.display_img(self.cur_imgidx, no_overwrite=True)
+        self.Fit()
         return True
-    def stop(sefl):
-        pass
-    def export_bounding_boxes(sefl):
-        pass
-    def checkCanMoveOn(self):
-        return True
-'''
+
+    def save_session(self, statefile=None):
+        """ Saves the current state of the current session. """
+        if statefile == None:
+            statefile = LabelPanel.STATE_FILE
+        state = {}
+        state['imagelabels'] = self.imagelabels
+        state['imagepaths'] = self.imagepaths
+        f = open(LabelPanel.STATE_FILE, 'wb')
+        pickle.dump(state, f)
+        f.close()
+
+    def display_img(self, idx, no_overwrite=False):
+        """Displays the image at idx, and allow the user to start labeling
+        it.
+        """
+        if not (idx < len(self.imagepaths)):
+            pdb.set_trace()
+        assert idx < len(self.imagepaths)
+        if not no_overwrite:
+            # First, store current input into our dict
+            old_imgpath = self.imagepaths[self.cur_imgidx]
+            cur_input = self.inputctrl.GetValue()
+            self.imagelabels[old_imgpath] = cur_input
+
+        self.cur_imgidx = idx
+        imgpath = self.imagepaths[self.cur_imgidx]
+        bitmap = wx.Bitmap(imgpath, type=wx.BITMAP_TYPE_PNG)
+        self.imgpatch.SetBitmap(bitmap)
+
+        self.inputctrl.SetValue(self.imagelabels[imgpath])
+        self.Fit()
+        
+    def export_labels(self):
+        """ Exports all labels to an output csvfile. """
+        f = open(self.outpath, 'w')
+        header = ('imgpath', 'label')
+        dictwriter = csv.DictWriter(f, header)
+        util_gui._dictwriter_writeheader(f, header)
+        for imgpath, label in self.imagelabels.iteritems():
+            row = {'imgpath': imgpath, 'label': label}
+            dictwriter.write_row(row)
+        f.close()
