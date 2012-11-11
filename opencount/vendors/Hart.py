967ecb937b2ce84a805cd3e9e533060a66a8dea0
Eric Kim <erickim555@gmail.com>
2012-11-10 18:22:33 -0800

    Hart: Bugfix, forgot to change OpenCount integration with the I2of5 newest changes.

--- opencount/vendors/Hart.py
+++ opencount/vendors/Hart.py
@@ -22,11 +22,14 @@ except NameError:
 TOP_GUARD_IMGP = pathjoin(MYDIR, 'hart_topguard.png')
 BOT_GUARD_IMGP = pathjoin(MYDIR, 'hart_botguard.png')
 
+TOP_GUARD_SKINNY_IMGP = pathjoin(MYDIR, 'hart_topguard_skinny.png')
+BOT_GUARD_SKINNY_IMGP = pathjoin(MYDIR, 'hart_botguard_skinny.png')
+
 class HartVendor(Vendor):
     def __init__(self):
         pass
     
-    def decode_image(self, imgpath, TOP_GUARD=None, BOT_GUARD=None):
+    def decode_image(self, imgpath, topbot_pairs):
         """ 
         Input:
             str IMGPATH:
@@ -36,10 +39,10 @@ class HartVendor(Vendor):
                 the location of each barcode:
                     [[str bc_i, ...], bool is_flip, [(x1,y1,x2,y2), ...]].
         """
-        decodes, isflipped, bbs = decode(imgpath, only_ul=True, TOP_GUARD=TOP_GUARD, BOT_GUARD=BOT_GUARD)
+        decodes, isflipped, bbs = decode(imgpath, topbot_pairs, only_ul=True)
         return decodes, isflipped, bbs
 
-    def decode_ballot(self, ballot, TOP_GUARD=None, BOT_GUARD=None):
+    def decode_ballot(self, ballot, topbot_guards):
         """
         Input:
             list BALLOT: List of image paths, together which correspond
@@ -52,7 +55,7 @@ class HartVendor(Vendor):
         """
         results = []
         for imgpath in ballot:
-            (bcs, isflipped, bbs) = self.decode_image(imgpath, TOP_GUARD=TOP_GUARD, BOT_GUARD=BOT_GUARD)
+            (bcs, isflipped, bbs) = self.decode_image(imgpath, topbot_guards)
             results.append((bcs, isflipped, bbs))
         return results
 
@@ -81,10 +84,16 @@ class HartVendor(Vendor):
         cur_pid = 0
         TOP_GUARD = cv.LoadImage(TOP_GUARD_IMGP, cv.CV_LOAD_IMAGE_GRAYSCALE)
         BOT_GUARD = cv.LoadImage(BOT_GUARD_IMGP, cv.CV_LOAD_IMAGE_GRAYSCALE)
+        TOP_GUARD_SKINNY = cv.LoadImage(TOP_GUARD_SKINNY_IMGP, cv.CV_LOAD_IMAGE_GRAYSCALE)
+        BOT_GUARD_SKINNY = cv.LoadImage(BOT_GUARD_SKINNY_IMGP, cv.CV_LOAD_IMAGE_GRAYSCALE)
+        topbot_pairs = [[TOP_GUARD, BOT_GUARD], [TOP_GUARD_SKINNY, BOT_GUARD_SKINNY]]
         for ballotid, imgpaths in ballots.iteritems():
-            decoded_results = self.decode_ballot(imgpaths, TOP_GUARD=TOP_GUARD, BOT_GUARD=BOT_GUARD)
+            decoded_results = self.decode_ballot(imgpaths, topbot_pairs)
             d_strs = []
             for i, (bcs, isflipped, bbs) in enumerate(decoded_results):
+                if 'ERR' in bcs[0]:
+                    print "..error on: {0}".format(imgpaths[i])
+                    continue
                 imgpath = imgpaths[i]
                 info = get_info(bcs)
                 info['isflip'] = isflipped
