499ff25173b32281e1d047f627bd97307abf8660
Eric Kim <erickim555@gmail.com>
2012-11-19 21:48:34 -0800

    Diebold_Raw: A few adjustments to the Diebold decoder to ease the OpenCount integration.

--- opencount/vendors/diebold_raw.py
+++ opencount/vendors/diebold_raw.py
@@ -1,17 +1,54 @@
+import os, sys, time, pdb
 import cv
-def decode(imagepath, template_path = "diebold-mark.jpg"):
+
+def decode(imgpath, template="diebold-mark.jpg"):
+    """
+    Input:
+        str IMGPATH: Path to voted ballot image.
+    Output:
+        (str decoding, bool isflip, list BBS)
+    """
+    if isinstance(template, str) or isinstance(template, unicode):
+        Itemp = cv.LoadImage(template, cv.CV_LOAD_IMAGE_GRAYSCALE)
+    else:
+        Itemp = template
+    I = cv.LoadImage(imgpath, cv.CV_LOAD_IMAGE_GRAYSCALE)
+    w, h = cv.GetSize(I)
+    isflip = False
+    # C_LOW, C_HIGH are lower/upper percentage to crop image by
+    c_low = 0.9268
+    c_high = 0.9909
+    y1 = int(round(c_low * h))
+    y2 = int(round(c_high * h))
+    cv.SetImageROI(I, (0, y1, w, int(y2-y1)))
+    decoding, bbs = decode_patch(I, Itemp)
+    if decoding in (None, '1'*32):
+        # Try Flipping the image, and try again.
+        c_low = 1.0 - c_high
+        c_high = 1.0 - c_low
+        y1 = int(round(c_low * h))
+        y2 = int(round(c_high*h))
+        cv.SetImageROI(I, (0, y1, w, int(y2-y1)))
+        cv.Flip(I, I, flipMode=-1)
+        isflip = True
+        decoding, bbs = decode_patch(I, Itemp)
+
+    if decoding == None:
+        return None, None, None
+    else:
+        return decoding, isflip, bbs
+
+def decode_patch(patch, template):
     """
     input: path to image, path to template
     output: decoded string, coordinates of black markings in form(x_topleft, y_topleft, x_bottomright, y_bottomright)
     """
-    image = cv.LoadImage(imagepath)
-    template = cv.LoadImage(template_path)
     w,h = cv.GetSize(template)
-    W,H = cv.GetSize(image)
+    W,H = cv.GetSize(patch)
     width = W - w + 1
     height = H - h + 1
     result = cv.CreateImage((width, height), 32, 1)
-    cv.MatchTemplate(image, template, result, cv.CV_TM_CCOEFF_NORMED)
+    cv.MatchTemplate(patch, template, result, cv.CV_TM_CCOEFF_NORMED)
     coords = []
     for x in range(width):
         for y in range(height):
@@ -41,18 +78,39 @@ def decode(imagepath, template_path = "diebold-mark.jpg"):
 
     for x,y in coords:
         string[int((x+spacing*.2) // spacing)] = "1"
-
+    out_str = ''.join(string)
 
     coords = [(x,y,x+w,y+h) for x,y in coords]
-    print "".join(string)
-    print coords
-    return string, coords
     
+    return out_str, coords
 
+def isimgext(f):
+    return os.path.splitext(f)[1].lower() in ('.png', '.jpg', '.jpeg', '.bmp')
 
+def main():
+    args = sys.argv[1:]
+    arg0 = args[0]
+    if isimgext(arg0):
+        imgpaths = [arg0]
+    else:
+        imgpaths = []
+        for dirpath, dirnames, filenames in os.walk(arg0):
+            for imgname in [f for f in filenames if isimgext(f)]:
+                imgpaths.append(os.path.join(dirpath, imgname))
 
+    t = time.time()
+    Itemp = cv.LoadImage("diebold-mark.jpg", cv.CV_LOAD_IMAGE_GRAYSCALE)
+    for imgpath in imgpaths:
+        decoding, isflip, bbs = decode(imgpath, Itemp)
+        if decoding == None:
+            print 'Error:', imgpath
+        else:
+            print "{0}: {1}".format(os.path.split(imgpath)[1], decoding)
+            print "    isflip={0}".format(isflip)
 
+    total_dur = time.time() - t
+    print "...Done ({0} s).".format(total_dur)
+    print "    Average Time Per Image: {0} s".format(total_dur / float(len(imgpaths)))
 
-
-
-
+if __name__ == '__main__':
+    main()
