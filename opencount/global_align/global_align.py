bee42b1c49376bc74e00bdda84ec599c95685d9e
Eric Kim <erickim555@gmail.com>
2013-01-13 21:17:47 -0800

    GlobalAlign: Added the 'align_strong' alignment strategy. doExtract: Modified the code to make it easier to (in the future) use the global_align.py alignment functions.

--- opencount/global_align/global_align.py
+++ opencount/global_align/global_align.py
@@ -8,20 +8,6 @@ import pixel_reg.imagesAlign as imagesAlign
 Functions that globally-align ballot images together.
 """
 
-def cropout_border(I, top, bot, left, right):
-    """ float TOP,BOT,LEFT,RIGHT are between [0.0, 1.0]. """
-    '''
-    h, w = I.shape
-    x1 = int(round(left*w))
-    y1 = int(round(top*h))
-    x2 = int(round(w - (right*w)))
-    y2 = int(round(h - (bot*h)))
-    Inew = I[y1:y2, x1:x2]
-    return np.copy(Inew)
-    '''
-    # TODO: Disable the cropping, due to adverse affects on Yolo
-    return I
-
 def align_image(Iref, I):
     """ Aligns I to IREF (e.g. 'global' alignment). Both IREF and I
     must be correctly 'flipped' before you pass it to this function.
@@ -40,8 +26,44 @@ def align_image(Iref, I):
     # Apply H to I to get the final alignment IREG.
     # NOTE: The cropping-out is currently DISABLED, due to adverse effects
     #       on Yolo. 
-    Icrop = cropout_border(I, 0.2, 0.2, 0.2, 0.2)
+    #Icrop = cropout_border(I, 0.2, 0.2, 0.2, 0.2)
+    Icrop = I
     H, Ireg_crop, err = imagesAlign.imagesAlign(Icrop, Iref, type='rigid', rszFac=0.25)
     Ireg = imagesAlign.imtransform(I, H)
     Ireg = np.nan_to_num(Ireg)
     return H, Ireg, err
+
+def cropout_stuff(I, top, bot, left, right):
+    """ Crops out some percentage from each side of I. """
+    h, w = I.shape
+    x1 = int(round(left*w))
+    y1 = int(round(top*h))
+    x2 = int(round(w - (right*w)))
+    y2 = int(round(h - (bot*h)))
+    Inew = I[y1:y2, x1:x2]
+    return np.copy(Inew)
+
+def correctH(I, H0):
+    T0=np.eye(3); T0[0,2]=I.shape[1]/2.0; T0[1,2]=I.shape[0]/2.0
+    T1=np.eye(3); T1[0,2]=-I.shape[1]/2.0; T1[1,2]=-I.shape[0]/2.0
+    H=np.dot(np.dot(T0,H0),T1)
+    return H
+
+def align_strong(I, Iref, scales=(0.15, 0.2, 0.25, 0.3)):
+    """ Alignment strategy: First, crop out 5% from each side of I.
+    Then, try a range of scales, and choose the alignment that 
+    minimizes the error.
+    """
+    Icrop = cropout_stuff(I, 0.05, 0.05, 0.05, 0.05)
+    H_best, Ireg_best, err_best = None, None, None
+    scale_best = None
+    for scale in scales:
+        H, Ireg, err = imagesAlign.imagesAlign(Icrop, Iref, fillval=1, type='rigid', rszFac=scale)
+        if err_best == None or err < err_best:
+            H_best = H
+            Ireg_best = Ireg
+            err_best = err
+            scale_best = scale
+    # Finally, apply H_BEST to I
+    Ireg = imagesAlign.imtransform(I, H_best)
+    return H_best, Ireg, err_best
